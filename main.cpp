3ed863cff706dcbd518f53f4075c46e8026106d7
felix_lara <larafelix2197@gmail.com>
2025-09-09 23:13:33 -0500

    Agregando extension para formatear el codigo

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "Hola, mundo desde C++ en VS Code y usando g++!" << endl;
-    return 0;
+  cout << "Hola, mundo desde C++ en VS Code y usando g++!" << endl;
+  return 0;
 }
